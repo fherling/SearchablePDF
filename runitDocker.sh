a1e14662e085c8c20681ae2d78f0213a0a7c9f3c
fherling <frank.herling@prodyna.com>
2023-10-25 07:42:27 +0200

    Adjust local run

--- runitDocker.sh
+++ runitDocker.sh
@@ -1,5 +1,12 @@
-# run the docker image with local directory mounted and delete the container after exit
-#mvn clean package
-docker build -f Dockerfile -t fherlingatpd/fherling-searchablepdf:latest .
 
-docker run --name fherling-searchablepdf --rm -v /Users/fherling/Documents/git/OCR/ocr-input:/ocr-input -v /Users/fherling/Documents/git/OCR/ocr-output:/ocr-output fherlingatpd/fherling-searchablepdf:latest
+mvn clean package
+
+
+docker build -f Dockerfile -t fherlingatpd/fherling-searchablepdf:latest .
+#docker buildx build --platform linux/amd64,linux/arm64/v8  -f Dockerfile -t fherlingatpd/fherling-searchablepdf:latest .
+
+docker run --name fherling-searchablepdf --rm -e AWS_ACCESS_KEY_ID=$ACCESS_KEY_ID -e AWS_SECRET_ACCESS_KEY=$ACCESS_KEY_SECRET -e AWS_DEFAULT_REGION=$USED_REGION -e AWS_REGION=$USED_REGION -v /Users/fherling/Documents/git/OCR/ocr-input:/ocr-input -v /Users/fherling/Documents/git/OCR/ocr-output:/ocr-output fherlingatpd/fherling-searchablepdf:latest
+
+
+#docker run --name fherling-searchablepdf --rm -e AWS_ACCESS_KEY_ID=$ACCESS_KEY_ID -e AWS_SECRET_ACCESS_KEY=$ACCESS_KEY_SECRET -e AWS_DEFAULT_REGION=$USED_REGION -e AWS_REGION=$USED_REGION -v /Users/fherling/Documents/git/OCR/ocr-input:/ocr-input -v /Users/fherling/Documents/git/OCR/ocr-output:/ocr-output ghcr.io/fherling/searchablepdf:latest
+
